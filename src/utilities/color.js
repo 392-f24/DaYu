0a2b39f643a55cf0ad1a9341203f528b93d3904c
Aining Li <annieli2025@u.northwestern.edu>
2024-11-17 14:13:23 -0600

    fix color

--- src/utilities/color.js
+++ src/utilities/color.js
@@ -1,5 +1,5 @@
 export const getChipColor = (category) => {
-  if (category === "pathways") return "primary";
+  if (category === "sidewalk") return "primary";
   if (category === "traffic") return "secondary";
   return "thirdColor";
 };
