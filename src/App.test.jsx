5e8ac08feab9162f5df55a3f807c1fc37ccec4ac
Isa Gonzalez <isaegonzalez03@gmail.com>
2024-11-17 13:21:44 -0600

    Removed app test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,19 +1,9 @@
-import {describe, expect, test} from 'vitest';
-import {fireEvent, render, screen} from '@testing-library/react';
-import App from './App';
+import { describe, expect, test } from "vitest";
+import { fireEvent, render, screen } from "@testing-library/react";
+import App from "./App";
 
-describe('counter tests', () => {
-    
-  test("Counter should be 0 at the start", () => {
-    render(<App />);
-    expect(screen.getByText('count is: 0')).toBeDefined();
+describe("empty tests", () => {
+  test("Placeholder for actual test", () => {
+    // render(<App />);
   });
-
-  test("Counter should increment by one when clicked", async () => {
-    render(<App />);
-    const counter = screen.getByRole('button');
-    fireEvent.click(counter);
-    expect(await screen.getByText('count is: 1')).toBeDefined();
-  });
-
 });
